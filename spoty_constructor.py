f69a148a0f1475ea01510402ae9e7205120a62f8
SammyJankins <neroragno@mail.ru>
2020-08-10 16:54:16 +0700

    Initial commit

---
+++ spoty_constructor.py
@@ -0,0 +1,114 @@
+import os
+from collections import defaultdict
+from time import sleep
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+from keys import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, APP_USERNAME
+
+
+# guide to get spotify client_id and secret:
+# https://developer.spotify.com/documentation/general/guides/app-settings/
+
+def valid_naming(line):
+    return ''.join(list(filter(lambda x: x.isalnum() or x not in ';:~@#$%^-_…(){}.,\'`', line)))
+
+
+def do_spoty_object():
+    return spotipy.Spotify(
+        auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                  client_secret=CLIENT_SECRET,
+                                  redirect_uri=REDIRECT_URI,
+                                  username=APP_USERNAME,
+                                  scope='user-library-read playlist-modify-public',
+                                  ))
+
+
+class SpPlaylist:
+
+    def __init__(self, filename, directory, spoty_obj):
+        self.filename = filename
+        self.directory = directory
+        self.spoty_obj = spoty_obj
+        self.track_links = []
+        self.not_found = []
+        self.pl_link = ''
+        self.pl_title = ''
+
+    def get_track_list(self):
+        with open(f'{self.directory}{self.filename}', mode='r', encoding='utf8') as track_list:
+            return [line[:-1] for line in track_list]
+
+    def scan(self, list_to_scan):
+        for line in list_to_scan:
+            artist, title = line.split(' ^/& ')
+            if 'из' in title:
+                title = title.split('из')[0]
+            result = self.spoty_obj.search(f'{artist} {title}')['tracks']['items']
+            if not result:
+                result = self.spoty_obj.search(f'{artist} {valid_naming(title)}')['tracks']['items']
+            if result:
+                for position in result:
+                    search_title = valid_naming(position['name'])
+                    condition_1 = artist.casefold() == position['artists'][0]['name'].casefold()
+                    condition_2 = valid_naming(title).casefold() == search_title.casefold()
+                    condition_3 = title.casefold() in position['name'].casefold()
+                    condition_4 = sorted(valid_naming(title).casefold().split()) == sorted(
+                        search_title.casefold().split())
+                    condition_5 = valid_naming(title).casefold() in search_title.casefold()
+                    condition_6 = search_title.casefold() in valid_naming(title).casefold()
+                    condition_7 = len(
+                        set(valid_naming(title).casefold().split()) & set(search_title.casefold().split())) >= 2
+                    if any((condition_1, condition_2, condition_3,
+                            condition_4, condition_5, condition_6, condition_7)):
+                        self.track_links.append(position['uri'])
+                        break
+            else:
+                print(f"Can't find track on Spotify: {artist} - {title}")
+                self.not_found.append(line)
+
+    def run(self):
+        self.pl_title = self.filename.split('.')[0]
+        track_list = self.get_track_list()
+        self.scan(track_list)
+        if not self.track_links:
+            self.pl_link = None
+            print(f"Didn't find any track for '{self.pl_title}'")
+            return
+        playlist = self.spoty_obj.user_playlist_create(APP_USERNAME, self.pl_title)
+        self.pl_link = playlist['external_urls']['spotify']
+        while len(self.track_links) > 100:
+            try:
+                self.spoty_obj.user_playlist_add_tracks(APP_USERNAME, playlist['id'], self.track_links[-100:])
+                self.track_links = self.track_links[:-100]
+            except Exception:
+                sleep(1)
+                continue
+        self.spoty_obj.user_playlist_add_tracks(APP_USERNAME, playlist['id'], self.track_links)
+        self.spoty_obj.user_playlist_unfollow(APP_USERNAME, playlist['id'])
+        print(f'Tracks missed: {len(self.not_found)}')
+
+
+class SpotifyUser:
+    def __init__(self, user_name):
+        self.username = user_name
+        self.directory = ''
+        self.track_links = []
+        self.not_found = defaultdict(list)
+        self.pl_links = dict()
+        self.spotify = None
+
+    def run(self):
+        self.spotify = do_spoty_object()
+        self.directory = f'users/{self.username}/'
+        sp_playlists = [SpPlaylist(filename=filename, directory=self.directory,
+                                   spoty_obj=self.spotify) for filename in os.listdir(self.directory)]
+        for pl in sp_playlists:
+            pl.run()
+        for title, url in self.pl_links.items():
+            print(f'Playlist: {title}, URL: {url}')
+
+
+if __name__ == '__main__':
+    pass
---
+++ yandex_pl_parser.py
@@ -0,0 +1,215 @@
+import argparse
+import os
+from collections import deque
+from pprint import pprint
+from threading import Thread
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from keys import YA_AGENT, YA_KEY, SP_USER
+from spoty_constructor import SpotifyUser
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('log-level=3')
+
+users_for_spoty = deque()
+
+
+class Track(Thread):
+
+    def __init__(self, collector, line, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.line = line
+        self.parent = None
+        self.result = None
+        self.collector = collector
+
+    def run(self):
+        self.parent = self.line.parent.next_sibling.find('a', attrs={'class': "deco-link"})
+        try:
+            self.collector.update((f'{self.parent.attrs["title"]} ^/& {self.line.attrs["title"]}',))
+        except:
+            return None
+
+
+def _ya_auth(driver):
+    while True:
+        try:
+            ya_auth_page = 'https://passport.yandex.ru/auth'
+            driver.get(ya_auth_page)
+            email = driver.find_element_by_id('passp-field-login')
+            email.send_keys(YA_AGENT)
+            log_btn = driver.find_element_by_class_name('Button2_view_action')
+            log_btn.click()
+            sleep(2)
+            pwd_field = driver.find_element_by_id('passp-field-passwd')
+            pwd_field.send_keys(YA_KEY)
+            pwd_btn = driver.find_element_by_class_name('Button2_size_auth-l')
+            pwd_btn.click()
+            sleep(2)
+            return
+        except Exception as exc:
+            print('Failed yandex authorization')
+
+
+class Playlist(Thread):
+
+    def __init__(self, pl_url, username, pl_title=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pl_url = pl_url
+        self.pl_title = pl_title
+        self.username = username
+        self.collector = set()
+        self.driver = None
+        self.done = False
+
+    def run(self):
+        page_y = _get_page_y(self.pl_url)
+        print(f'page y : {page_y}')
+        self.driver = webdriver.Chrome(options=options)
+        _ya_auth(self.driver)
+        self.driver.get(self.pl_url)
+        sleep(4)
+        if page_y > 3000:
+            self.driver.set_window_size(1000, 3000)
+            heights = range(3000, page_y, 5000)
+            for scroll_to in heights:
+                self.driver.execute_script(f"window.scrollTo(0, {scroll_to});")
+                self._eval_rendered_tracks(self.collector)
+            else:
+                self.driver.execute_script(f"window.scrollTo(0, {page_y - 1000});")
+                self._eval_rendered_tracks(self.collector)
+        else:
+            self.driver.execute_script(f"window.scrollTo(0, {2000});")
+            self._eval_rendered_tracks(self.collector)
+
+        self.driver.quit()
+        print(self.pl_title, len(self.collector))
+        self._save_result()
+
+    def _eval_rendered_tracks(self, collector):
+        sleep(3)
+        content = self.driver.page_source
+        soup = BeautifulSoup(content, features="lxml")
+        tracks = [Track(collector, tag) for tag in soup.find_all("div", attrs={"class": "d-track__name"})]
+        for track in tracks:
+            track.start()
+        for track in tracks:
+            track.join()
+
+    def _save_result(self):
+        valid_filename = ''.join(list(filter(lambda x: x.isalnum() or x in ' ~@#$%^-_(){}\'`', self.pl_title)))
+        valid_filename = f'{valid_filename}.txt'
+        with open(f'users/{self.username}/{valid_filename}', mode='a', encoding='utf8') as file:
+            for value in self.collector:
+                file.write(f'{value}\n')
+            self.done = True
+
+
+class User:
+
+    def __init__(self, user_url):
+        self.user_url = user_url
+        self.ya_url = 'https://music.yandex.ru'
+        self.playlists = []
+        self.driver = None
+        self.username = ''
+        self.done = False
+
+    def run(self):
+        self.username = self.user_url.split("/")[-1]
+        os.makedirs(f'users/{self.username}', exist_ok=True)
+        playlists_url = f'{self.user_url}/playlists/'
+        self.driver = webdriver.Chrome(options=options)
+        _ya_auth(self.driver)
+        self.driver.get(playlists_url)
+        sleep(3)
+        content = self.driver.page_source
+        self.driver.quit()
+        soup = BeautifulSoup(content, features="lxml")
+        playlists = soup.find_all("a", attrs={"class": "d-link deco-link playlist__title-cover"})
+        titles = soup.find_all("div", attrs={"class": "playlist__title deco-typo typo-main"})
+        playlist_links = [tag.attrs["href"] for tag in playlists]
+        playlist_titles = [tag.attrs["title"] for tag in titles]
+        pprint(playlist_titles)
+        pprint(playlist_links)
+        attrs_dict = [{'pl_url': f'{self.ya_url}{value[0]}', 'pl_title': value[1], 'username': self.username} for value
+                      in
+                      zip(playlist_links, playlist_titles)]
+        ya_playlists = [Playlist(**items) for items in attrs_dict]
+
+        for pl in ya_playlists:
+            pl.start()
+        for pl in ya_playlists:
+            pl.join()
+
+        while not all([pl.done for pl in ya_playlists]):
+            sleep(10)
+            continue
+        else:
+            self.done = True
+
+
+# To use webdriver.Chrome() you should download the chromedriver
+# and place it to the script directory. Or place it wherever you like and
+# and specify it's location like webdriver.Chrome('driver/location')
+# link for download: https://sites.google.com/a/chromium.org/chromedriver/downloads
+# !!! to download correct version - check it in your chrome's options
+
+
+def _get_page_y(url):
+    driver = webdriver.Chrome(options=options)
+    _ya_auth(driver)
+    driver.get(url)
+    sleep(3)
+    height = driver.execute_script("return document.documentElement.scrollHeight")
+    driver.execute_script(f"window.scrollTo(0, {str(height)});")
+    sleep(3)
+    driver.quit()
+    return height
+
+
+def _parse_object(url):
+    url_values = url.split('/')
+    if url_values[-2] == 'playlists':
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, features='lxml')
+        div = soup.find('div', attrs={'class': 'd-generic-page-head__main-top'})
+        title = div.find('h1', attrs={'class': 'page-playlist__title'}).text
+        os.makedirs(f'users/{url_values[-3]}', exist_ok=True)
+        return Playlist(pl_url=url, username=url_values[-3], pl_title=title)
+    elif url_values[-2] == 'users':
+        return User(user_url=url)
+    else:
+        return None
+
+
+def _process_cmd(args):
+    # user_url = 'https://music.yandex.ru/users/sammyjankins'
+    p_obj = _parse_object(args.yandex)
+    if isinstance(p_obj, (User, Playlist)):
+        p_obj.run()
+    else:
+        print('Got None')
+    spoty_user = SpotifyUser(SP_USER)
+    spoty_user.run()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yandex', action='store', dest='yandex', required=True, help='Yandex profile page')
+    args = parser.parse_args()
+    _process_cmd(args)
+
+
+# python yandex_pl_parser.py --yandex https://music.yandex.ru/users/sammyjankins
+
+
+if __name__ == '__main__':
+    main()
